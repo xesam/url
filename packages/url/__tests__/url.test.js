6ccbc0198c488e27df0a8cf45c6047bbc32fa53c
xesam <xesam@outlook.com>
2022-10-16 18:17:46 +0800

    url test /fix

--- packages/url/__tests__/url.test.js
+++ packages/url/__tests__/url.test.js
@@ -6,7 +6,7 @@ describe('url', () => {
         expect(urls).toStrictEqual({});
     });
 
-    test('when url is a blank string then return all undefined', () => {
+    test('when url is a blank string then all parts are undefined', () => {
         const urls = url('  ');
         expect(urls).toStrictEqual({
             protocol: undefined,
@@ -21,37 +21,37 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{hostname} then return all undefined', () => {
+    test('when url is {protocol}://{hostname} then return protocol, host, hostname', () => {
         const urls = url('https://xesam.github.io');
         expect(urls).toStrictEqual({
+            protocol: 'https:',
             auth: undefined,
+            host: 'xesam.github.io',
+            hostname: 'xesam.github.io',
             port: undefined,
             pathname: undefined,
             search: undefined,
             query: undefined,
-            hash: undefined,
-            protocol: 'https:',
-            host: 'xesam.github.io',
-            hostname: 'xesam.github.io'
+            hash: undefined
         });
     });
 
-    test('when url is {protocol}://{hostname}:{port} then return protocol、hostname', () => {
+    test('when url is {protocol}://{hostname}:{port} then return protocol,hostname,port', () => {
         const urls = url('https://xesam.github.io:443');
         expect(urls).toStrictEqual({
+            protocol: 'https:',
             auth: undefined,
+            host: 'xesam.github.io:443',
+            hostname: 'xesam.github.io',
             port: '443',
             pathname: undefined,
             search: undefined,
             query: undefined,
-            hash: undefined,
-            protocol: 'https:',
-            host: 'xesam.github.io:443',
-            hostname: 'xesam.github.io'
+            hash: undefined
         });
     });
 
-    test('when url is {protocol}://{name}:{password}@{hostname}:{port} then return protocol、auth、hostname、port', () => {
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port} then return protocol,auth,host,hostname,port', () => {
         const urls = url('https://admin:root@xesam.github.io:443');
         expect(urls).toStrictEqual({
             auth: 'admin:root',
@@ -66,7 +66,7 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path} then return protocol、auth、hostname、port、path', () => {
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path} then return protocol,auth,host,hostname,port,path', () => {
         const urls = url('https://admin:root@xesam.github.io:80/');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -81,37 +81,52 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{name}:{password}@{hostname}:{port}?{query} then return protocol、auth、hostname、port、query', () => {
-        const urls = url('https://admin:root@xesam.github.io:80?name=xesam');
-        expect(urls).toStrictEqual({
-            auth: 'admin:root',
-            port: '80',
-            pathname: undefined,
-            search: '?name=xesam',
-            query: 'name=xesam',
-            hash: undefined,
-            protocol: 'https:',
-            host: 'xesam.github.io:80',
-            hostname: 'xesam.github.io'
-        });
-    });
-
-    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path}?{query} then return protocol、auth、hostname、port、query', () => {
-        const urls = url('https://admin:root@xesam.github.io:80/abc/def?name=xesam');
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path}?{query} then return protocol,auth,hostname,port,path,query', () => {
+        const urls = url('https://admin:root@xesam.github.io:80/p/a/t/h?name=xesam');
         expect(urls).toStrictEqual({
             protocol: 'https:',
             auth: 'admin:root',
             host: 'xesam.github.io:80',
             hostname: 'xesam.github.io',
             port: '80',
-            pathname: '/abc/def',
+            pathname: '/p/a/t/h',
             search: '?name=xesam',
             query: 'name=xesam',
             hash: undefined
         });
     });
 
-    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path}?{query}{hash} then return protocol、auth、hostname、port、path、search、query、hash', () => {
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path}?{query}{hash} then return protocol,auth,hostname,port,path,query,hash', () => {
+        const urls = url('https://admin:root@xesam.github.io:80/p/a/t/h?name=xesam#the-hash');
+        expect(urls).toStrictEqual({
+            protocol: 'https:',
+            auth: 'admin:root',
+            host: 'xesam.github.io:80',
+            hostname: 'xesam.github.io',
+            port: '80',
+            pathname: '/p/a/t/h',
+            search: '?name=xesam',
+            query: 'name=xesam',
+            hash: '#the-hash'
+        });
+    });
+
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port}?{query} then return protocol,auth,hostname,port,query', () => {
+        const urls = url('https://admin:root@xesam.github.io:80?name=xesam');
+        expect(urls).toStrictEqual({
+            protocol: 'https:',
+            auth: 'admin:root',
+            host: 'xesam.github.io:80',
+            hostname: 'xesam.github.io',
+            port: '80',
+            pathname: undefined,
+            search: '?name=xesam',
+            query: 'name=xesam',
+            hash: undefined
+        });
+    });
+
+    test('when url is {protocol}://{name}:{password}@{hostname}:{port}{path}?{query}{hash} then return protocol,auth,hostname,port,path,search,query,hash', () => {
         const urls = url('https://xesam.github.io:80/abc/def?name=xesam&age=18#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -126,7 +141,7 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{hostname}{path}?{query}{hash} then return protocol、hostname、path、query、hash', () => {
+    test('when url is {protocol}://{hostname}{path}?{query}{hash} then return protocol,hostname,path,query,hash', () => {
         const urls = url('https://xesam.github.io/ab/c/?name=xesam&age=18#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -141,7 +156,7 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{hostname}?{query}{hash} then return protocol、hostname、search、query、hash', () => {
+    test('when url is {protocol}://{hostname}?{query}{hash} then return protocol,hostname,search,query,hash', () => {
         const urls = url('https://xesam.github.io?name=xesam&age=18#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -156,7 +171,7 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{password}@{hostname}{path}{hash} then return protocol、hostname、path、hash', () => {
+    test('when url is {protocol}://{password}@{hostname}{path}{hash} then return protocol,hostname,path,hash', () => {
         const urls = url('https://xesam.github.io/#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -171,7 +186,7 @@ describe('url', () => {
         });
     });
 
-    test('when url is {protocol}://{hostname}{hash} then return protocol、hostname、hash', () => {
+    test('when url is {protocol}://{hostname}{hash} then return protocol,hostname,hash', () => {
         const urls = url('https://xesam.github.io#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: 'https:',
@@ -186,8 +201,8 @@ describe('url', () => {
         });
     });
 
-    test('when url is {path}{hash} then return path、hash', () => {
-        const urls = url('/abc#t=123456?a[0]=100&a[1]=200&b=');
+    test('when url is {path} then return path', () => {
+        const urls = url('/abc');
         expect(urls).toStrictEqual({
             protocol: undefined,
             auth: undefined,
@@ -197,26 +212,11 @@ describe('url', () => {
             pathname: '/abc',
             search: undefined,
             query: undefined,
-            hash: '#t=123456?a[0]=100&a[1]=200&b='
+            hash: undefined
         });
     });
 
-    test('when url is {path}?{query}{hash} then return path、search、query、hash', () => {
-        const urls = url('/abc?name=xesam#t=123456?a[0]=100&a[1]=200&b=');
-        expect(urls).toStrictEqual({
-            protocol: undefined,
-            auth: undefined,
-            host: undefined,
-            hostname: undefined,
-            port: undefined,
-            pathname: '/abc',
-            search: '?name=xesam',
-            query: 'name=xesam',
-            hash: '#t=123456?a[0]=100&a[1]=200&b='
-        });
-    });
-
-    test('when url is {path}? then return path、search、query', () => {
+    test('when url is {path}? then return path,search,query', () => {
         const urls = url('/abc?');
         expect(urls).toStrictEqual({
             protocol: undefined,
@@ -231,19 +231,48 @@ describe('url', () => {
         });
     });
 
-    test('when url is {search} then return search、query', () => {
-        const urls = url('?');
+    test('when url is {path}?{query} then return path,search,query', () => {
+        const urls = url('/abc?name=xesam#t=123456?a[0]=100&a[1]=200&b=');
         expect(urls).toStrictEqual({
             protocol: undefined,
             auth: undefined,
             host: undefined,
             hostname: undefined,
             port: undefined,
-            pathname: undefined,
-            search: '?',
-            query: '',
-            hash: undefined
+            pathname: '/abc',
+            search: '?name=xesam',
+            query: 'name=xesam',
+            hash: '#t=123456?a[0]=100&a[1]=200&b='
         });
     });
 
+    test('when url is {path}?{query}{hash} then return path,search,query,hash', () => {
+        const urls = url('/abc?name=xesam#t=123456?a[0]=100&a[1]=200&b=');
+        expect(urls).toStrictEqual({
+            protocol: undefined,
+            auth: undefined,
+            host: undefined,
+            hostname: undefined,
+            port: undefined,
+            pathname: '/abc',
+            search: '?name=xesam',
+            query: 'name=xesam',
+            hash: '#t=123456?a[0]=100&a[1]=200&b='
+        });
+    });
+
+    test('when url is {path}{hash} then return path,hash', () => {
+        const urls = url('/abc#t=123456?a[0]=100&a[1]=200&b=');
+        expect(urls).toStrictEqual({
+            protocol: undefined,
+            auth: undefined,
+            host: undefined,
+            hostname: undefined,
+            port: undefined,
+            pathname: '/abc',
+            search: undefined,
+            query: undefined,
+            hash: '#t=123456?a[0]=100&a[1]=200&b='
+        });
+    });
 });
